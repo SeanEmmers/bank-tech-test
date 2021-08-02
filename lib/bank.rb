9cbf47fc79b72cfe606d9340c723b3c0aff4e4ad
Sean Phillips <seanphillips@seans-air.lan>
2021-08-02 18:46:54 +0100

    passing first test

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
+# bank class
 class Bank
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def bank_balance
+    balance
+  end
+
+  private
+
+  attr_accessor :balance
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'bank'
 
 describe Bank do
-
+  describe '.deposit' do
+    it 'adds the amount deposited to the total bank balance' do
+      subject.deposit(50)
+      expect(subject.bank_balance).to eq 50
+    end
+  end
 end
