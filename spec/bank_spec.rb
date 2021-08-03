96bf4c1b202d6b79b063e5e34935c4f0499ad795
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 10:07:09 +0100

    saving date of deposit

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -8,6 +8,12 @@ describe Bank do
       subject.deposit(50)
       expect(subject.bank_balance).to eq 50
     end
+
+    it 'saves the date that the money was deposited' do
+      subject.deposit(50)
+      time = Time.now.strftime("%d/%m/%Y")
+      expect(subject.statement).to eq [ {time => {deposit: 50} } ]
+    end
   end
 
   describe '.withdrawl' do
@@ -21,13 +27,20 @@ describe Bank do
       subject.deposit(10)
       expect {subject.withdrawl(20)}.to raise_error "You cannot withdraw more than your total balance" 
     end
+
+    it 'saves the date that the money was withdrawn' do
+      subject.withdrawl(50)
+      time = Time.now.strftime("%d/%m/%Y")
+      expect(subject.statement).to eq [ {time => {withdawl: 50} } ]
+    end
   end
 
   describe '.statement' do
     it 'returns a history of transactions in a readable manor' do
       subject.deposit(50)
       subject.withdrawl(30)
-      expect(subject.statement).to eq [ {deposit: 50}, {withdrawl: 30} ]
+      time = Time.now.strftime("%d/%m/%Y")
+      expect(subject.statement).to eq [ {time => {deposit: 50} }, {withdrawl: 30} ]
     end
   end
 end
