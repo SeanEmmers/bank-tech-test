29e726af6ff0f879fb3a4ca21827eb08b6995c5b
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 12:48:27 +0100

    statement method now prints out human readable statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -9,18 +9,31 @@ class Bank
 
   def deposit(amount)
     @balance += amount
-    @transaction_history << { Time.now.strftime("%d/%m/%Y") => {deposit: amount} }
+    @transaction_history << { date: Time.now.strftime("%d/%m/%Y"), action: :deposit, amount: amount, balance: @balance }
   end
 
   def withdrawl(amount)
     raise "You cannot withdraw more than your total balance" if (@balance - amount) < 0
     @balance -= amount
-    @transaction_history << { Time.now.strftime("%d/%m/%Y") => {withdrawl: amount} }
+    @transaction_history << { date: Time.now.strftime("%d/%m/%Y"), action: :withdrawl, amount: amount, balance: @balance}
   end
 
   def statement
-    transaction_history
+    puts "date || credit || debit || balance \n" 
+    i = transaction_history.count - 1
+     until i == -1 do
+      puts "#{transaction_history[i][:date]} || #{credit?(i)} || #{debit?(i)} ||  #{transaction_history[i][:balance]} "
+      i -= 1
+    end
   end 
+
+  def credit?(trns_nmbr)
+    transaction_history[trns_nmbr][:action] == :withdrawl ? transaction_history[trns_nmbr][:amount] : ""
+  end
+
+  def debit?(trns_nmbr)
+    transaction_history[trns_nmbr][:action] == :deposit ? transaction_history[trns_nmbr][:amount] : ""
+  end
 
   def bank_balance
     balance
@@ -29,4 +42,5 @@ class Bank
   private
 
   attr_accessor :balance, :transaction_history
+
 end
