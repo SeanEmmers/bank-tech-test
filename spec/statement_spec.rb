f0edf4982bc9f023b45ce8f510d9d600f7e55892
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 17:10:14 +0100

    transaction history test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,5 +4,10 @@ require 'statement'
 
 describe Statement do
   describe '.print_statement' do
+
+    it 'displays the statement correctly' do
+      transaction_history = double('transaction_history')
+      expect(subject.print_statement(transaction_history)).to eq 
+    end
   end
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -3,6 +3,16 @@
 require 'transaction_history'
 
 describe TransactionHistory do
+  let(:transaction_class) { double('transaction class', new: 'transaction') }
+  let(:sum) {50}
+  let(:action) {:deposit}
+  let(:balance) {100}
+  subject(:transaction_history) { described_class.new(transaction_class) }
+
   describe '.add_transaction' do
+    it 'adds a new transaction' do
+    transaction_history.add_transaction(sum, action, balance)
+    expect(transaction_history.transactions).to eq [transaction_class.new]
+    end
   end
 end
