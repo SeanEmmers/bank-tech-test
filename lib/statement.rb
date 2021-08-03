d3bdc6140e735ebb6bcf80931030ad5ddd7a828d
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 15:18:45 +0100

    refactoring statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,9 +2,9 @@ class Statement
 
   def print_statement(transaction_history)
     puts "date || credit || debit || balance \n" 
-    i = transaction_history.transaction_history.count - 1
-     until i == -1 do
-      puts " #{transaction_history.transaction_history[i].time} || #{transaction_history.transaction_history[i].credit?} || #{transaction_history.transaction_history[i].debit?} ||  #{transaction_history.transaction_history[i].balance} "
+    i = transaction_history.count - 1
+     until i < 0 do
+      puts " #{transaction_history[i].time} || #{transaction_history[i].credit?} || #{transaction_history[i].debit?} ||  #{transaction_history[i].balance} "
       i -= 1
     end
   end 
--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -3,14 +3,14 @@ require_relative 'transaction'
 class TransactionHistory
 
   def initialize(transaction = Transaction)
-    @transaction_history = []
+    @transactions = []
     @transaction = transaction
   end
 
   def add_transaction(sum, action, balance)
-    transaction_history << transaction.new(sum, action, balance)
+    transactions << transaction.new(sum, action, balance)
   end
 
-  attr_accessor :transaction_history, :transaction
+  attr_accessor :transactions, :transaction
 
 end
