2192d08a098a52c44e76d6564be02c38c60b760f
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 19:26:11 +0100

    atm tests

--- spec/ATM_spec.rb
+++ spec/ATM_spec.rb
@@ -7,13 +7,20 @@ describe ATM do
   let(:transaction) { double('transaction') }
 
   let(:atm) { described_class.new }
+
+  before(:each) {
+    atm.deposit(500)
+  }
   describe '.deposit' do
     it 'adds the amount deposited to the total bank balance' do
+      expect(atm.show_balance).to eq 500
     end
   end
 
   describe '.withdraw' do
     it 'withdraws the correct amount from the bank balance' do
+      atm.withdraw(200)
+      expect(atm.show_balance).to eq 300
     end
 
     it 'does not let you withdraw below zero' do
