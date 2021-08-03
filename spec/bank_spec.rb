4274c4a265e44a8bc3163b03517e716562b08c37
Sean Phillips <seanphillips@seans-air.lan>
2021-08-03 10:14:04 +0100

    adding a before hook for depositing money

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,46 +3,46 @@
 require 'bank'
 
 describe Bank do
-  describe '.deposit' do
-    it 'adds the amount deposited to the total bank balance' do
-      subject.deposit(50)
-      expect(subject.bank_balance).to eq 50
+
+  context 'Money has already been deposited' do
+
+    before(:each) { subject.deposit(50) }
+
+    describe '.deposit' do
+      it 'adds the amount deposited to the total bank balance' do
+        expect(subject.bank_balance).to eq 50
+      end
+
+      it 'saves the date that the money was deposited' do
+        time = Time.now.strftime("%d/%m/%Y")
+        expect(subject.statement).to eq [ {time => {deposit: 50} } ]
+      end
     end
 
-    it 'saves the date that the money was deposited' do
-      subject.deposit(50)
-      time = Time.now.strftime("%d/%m/%Y")
-      expect(subject.statement).to eq [ {time => {deposit: 50} } ]
-    end
-  end
+    describe '.withdrawl' do
+      it 'withdrawls the correct amount from the bank balance' do
+        subject.withdrawl(30)
+        expect(subject.bank_balance).to eq 20
+      end
 
-  describe '.withdrawl' do
-    it 'withdrawls the correct amount from the bank balance' do
-      subject.deposit(50)
-      subject.withdrawl(30)
-      expect(subject.bank_balance).to eq 20
-    end
+      it 'does not let you withdraw below zero' do
+        expect {subject.withdrawl(60)}.to raise_error "You cannot withdraw more than your total balance" 
+        expect(subject.bank_balance).to eq 50
+      end
 
-    it 'does not let you withdraw below zero' do
-      subject.deposit(10)
-      expect {subject.withdrawl(20)}.to raise_error "You cannot withdraw more than your total balance" 
-      expect(subject.bank_balance).to eq 10
+      it 'saves the date that the money was withdrawn' do
+        subject.withdrawl(10)
+        time = Time.now.strftime("%d/%m/%Y")
+        expect(subject.statement).to eq [ {time => {deposit: 50} }, {time => {withdrawl: 10} } ]
+      end
     end
 
-    it 'saves the date that the money was withdrawn' do
-      subject.deposit(20)
-      subject.withdrawl(10)
-      time = Time.now.strftime("%d/%m/%Y")
-      expect(subject.statement).to eq [ {time => {deposit: 20} }, {time => {withdrawl: 10} } ]
-    end
-  end
-
-  describe '.statement' do
-    it 'returns a history of transactions in a readable manor' do
-      subject.deposit(50)
-      subject.withdrawl(30)
-      time = Time.now.strftime("%d/%m/%Y")
-      expect(subject.statement).to eq [ {time => {deposit: 50} }, {time => {withdrawl: 30} } ]
+    describe '.statement' do
+      it 'returns a history of transactions in a readable manor' do
+        subject.withdrawl(30)
+        time = Time.now.strftime("%d/%m/%Y")
+        expect(subject.statement).to eq [ {time => {deposit: 50} }, {time => {withdrawl: 30} } ]
+      end
     end
   end
 end
