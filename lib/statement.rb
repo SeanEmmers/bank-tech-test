4fac172837fcc67c5fe2ab3048047d806a7190f1
Sean Phillips <seanphillips@seans-air.lan>
2021-08-05 15:28:37 +0100

    refactoring statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,22 +3,30 @@
 # statement class
 class Statement
   def title
-    "    date    || credit || debit || balance"
+    "    date    || credit || debit  || balance"
   end
 
   def print_statement(transactions)
     puts title
     transactions.reverse_each do |transaction|
-      puts " #{transaction.time.strftime('%d:%m:%Y')} ||   #{check_credit(transaction)}  ||  #{check_debit(transaction)}  ||   #{transaction.balance} "
+      puts " #{display_time(transaction.time)} || #{check_credit(transaction)} || #{check_debit(transaction)} || #{display_amount(transaction.balance)} "
     end
   end
 
+  def display_time(time)
+    time.strftime('%d:%m:%Y')
+  end
+
   def check_credit(transaction)
-    transaction.credit? ? transaction.amount : '   '
+    transaction.credit? ? display_amount(transaction.amount) : '      '
   end
 
   def check_debit(transaction)
-    transaction.debit? ? transaction.amount : '   '
+    transaction.debit? ? display_amount(transaction.amount) : '      '
+  end
+
+  def display_amount(amount)
+    '%.2f' % amount
   end
 
 end
